16b7e632630f43a17a217dbadb41d4de010510ea
Will Rubin <willrubin1@gmail.com>
2023-10-22 19:07:25 -0400

    added stats view

--- client/ExerciseTracker/src/router/index.ts
+++ client/ExerciseTracker/src/router/index.ts
@@ -22,10 +22,12 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/stats',
+      name: 'statistics',
+      component: () => import('../views/StatsView.vue')
     },
     {
       path: '/myexercises',
