076bdeed11b62bf823895209dea4755555d6d024
Will Rubin <willrubin1@gmail.com>
2023-11-29 15:20:32 -0500

    added path & postController to index

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 //express.js
 
+const path = require('path');
+
 const express = require('express');
+
 const postController = require('./controllers/posts.js');
+
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app
+    .use('/', express.static(path.join(__dirname, '../client/dist')))
+
+    .use(express.json())
+
+    .use('/api/v1/posts', postController)
+
+    .get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    })
+
 
 console.log(`Server listening on ${PORT}`);
 
