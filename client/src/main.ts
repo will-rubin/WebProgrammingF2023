8d32786575ff8eb64770deb05761d70695cf5d5a
Will Rubin <willrubin1@gmail.com>
2023-11-29 21:35:35 -0500

    toast

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import "vue-toastification/dist/index.css";
 
 import { createApp } from 'vue'
+import Toast from "vue-toastification";
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(router).use(Toast, {})
 
 app.mount('#app')
--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -1,11 +1,19 @@
 import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';  
 import * as myFetch from './myFetch';
 import { type User, getUserByEmail } from './users';
 
+const toast = useToast();
+
 const session = reactive({
     user: null as User | null,
-    redirectURL: null as string | null
+    redirectURL: null as string | null,
+    messages: [] as {
+        type: string,
+        text: string
+    }[]
+    
 });
 
 export function api(action: string, body: string) {
@@ -36,4 +44,10 @@ export function useLogin(){
             alert('Login failed');
         }
     }
+}
+
+export function showError(err:any) {
+    console.error(err);
+    session.messages.push({ type: "error", text: err.message ?? err });
+    toast.error( err.message ?? err ); 
 }
