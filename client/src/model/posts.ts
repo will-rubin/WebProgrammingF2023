4a8a7e77db9ff728ec0cc07f1bbeb18771cbf87a
Will Rubin <willrubin1@gmail.com>
2023-12-06 12:56:19 -0500

    updating client posts and users

---
+++ client/src/model/posts.ts
@@ -0,0 +1,31 @@
+import e from "express";
+import { api } from "./session";
+
+
+export interface Post {
+  id: number
+  timestamp: string
+  location: string
+  caption: string
+  imageURL: string
+  distance: number
+  duration: number
+  author: string
+  fullName: string
+}
+
+export async function getPosts(): Promise<Post[]> {
+  return await api("posts");
+}
+
+export async function getPost(id: number): Promise<Post> {
+  return await api(`posts/${id}`);
+}
+
+export async function createPost(post: Post): Promise<Post> {
+  return await api("posts", post, "POST");
+}
+
+export async function updatePost(post: Post): Promise<Post> {
+  return await api(`posts/${post.id}`, post, "PATCH");
+}
--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -2,47 +2,25 @@ import { api } from './session';
 
 export interface User {
   id: number
-  firstName: string
-  lastName: string
+  fullName: string
   email: string
   password: string
   role: string
   imageURL: string
 }
 
-//Change as of 11/27/2023: We will now export a const users that is a copy of the data.users array.
-//Now, when we call addUsers, we will push to the users array, and not the data.users array.
-//Which will help us represent the users array in real time across the app, insha'Allah.
-
-
-export function getUsers(): Promise<User[]> {
-  return api("users", "");
+export async function getUsers(): Promise<User[]> {
+  return await api("users");
 }
 
-export function addUser(user: User): Promise<User> {
-  return api("users", JSON.stringify(user));
-}
-
-export function deleteUser(user: User) {
-  const index = users.findIndex( x => x.id === user.id);
-  users.splice(index, 1);
+export async function getUser(id: number): Promise<User | undefined> {
+  const users = await getUsers();
+  return users.find(u => u.id === id);
 }
 
 
-export function editUser(user: User, newid: number, firstName: string, lastName: string, email: string, password: string, role: string, imageURL: string) {
-  user.id = newid;
-  user.firstName = firstName;
-  user.lastName = lastName;
-  user.email = email;
-  user.password = password;
-  user.role = role;
-  user.imageURL = imageURL;
-}
 
 
-export function getUserByEmail(email: string): User | undefined {
-  return getUsers().find( x => x.email === email);
-}
 
 
 
