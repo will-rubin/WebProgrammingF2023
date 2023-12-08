3b83a065110a0d80dfb7a613d6ac31aeeca7ddf8
Will Rubin <willrubin1@gmail.com>
2023-12-07 22:05:01 -0500

    removed polyfill

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
     vue(),
     vueJsx(),
   ],
